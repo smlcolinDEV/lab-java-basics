0a19761247e130c26f387adcb391aa372c2823ee
Motodies <39949690+Motodies@users.noreply.github.com>
2025-03-29 01:37:37 +0100

    First commit

--- src/Employee.java
+++ src/Employee.java
@@ -12,26 +12,20 @@ public class Employee {
     public String getName() {
         return name;
     }
-
-    public int getAge() {
-        return age;
-    }
-
-    public int getSalary() {
-        return salary;
-    }
-
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+    public int getAge() {return age;}
+    public void setAge(int age) {this.age = age;}
 
+    public int getSalary() {
+        return salary;
+    }
     public void setSalary(int salary) {
         this.salary = salary;
     }
+
     public void updateSalary(int newSalary){
         this.salary = newSalary;
     }
